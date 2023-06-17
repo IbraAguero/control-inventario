49b8078ad1624f83700b2681fd2a3c0f9ca679cf
Nico <nmaxy2012@gmail.com>
2023-06-16 23:33:16 -0300

    add form update password

---
+++ src/scenes/global/FormUpdatePassword.jsx
@@ -0,0 +1,186 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Box,
+  FormControl,
+  TextField,
+  FormHelperText,
+  Alert,
+} from "@mui/material";
+import { Formik, Form, ErrorMessage, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { useSnackbar } from "notistack";
+
+const validationSchema = Yup.object().shape({
+  currentPassword: Yup.string().required("Campo requerido"),
+  newPassword: Yup.string()
+    .min(8, "Debe tener al menos 8 caracteres")
+    .required("Campo requerido"),
+  confirmNewPassword: Yup.string()
+    .oneOf([Yup.ref("newPassword"), null], "Las contraseñas no coinciden")
+    .required("Campo requerido"),
+});
+
+const FormUpdatePassword = ({ openDialog, handleCloseDialog }) => {
+  const { enqueueSnackbar } = useSnackbar();
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const username = localStorage.getItem("username");
+
+  const handleSubmit = async (values) => {
+    try {
+      const newValues={...values, username}
+      console.log(newValues)
+      const response = await axios.post(
+        "http://localhost:8000/api/update-password",
+        newValues
+      );
+      const data = response.data;
+      if (data.message === 'Contraseña actualizada exitosamente') {
+        handleCloseDialog(false);
+        setSuccessMessage(data.message);
+        enqueueSnackbar(data.message, { variant: "success" });
+      } else {
+        setErrorMessage(data.message);
+        setTimeout(() => {
+          setErrorMessage("");
+        }, 5000);
+      }
+    } catch (error) {
+      console.error("Error al cambiar la contraseña:", error);
+    }
+  };
+
+  return (
+    <Dialog open={openDialog} onClose={()=>handleCloseDialog(false)}>
+      <DialogTitle
+        sx={{
+          background: "#444",
+          fontSize: "20px",
+          fontWeight: "600",
+        }}
+      >
+        Cambiar de contraseña
+      </DialogTitle>
+      <DialogContent>
+        <Formik
+          initialValues={{
+            currentPassword: "",
+            newPassword: "",
+            confirmNewPassword: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values)=>handleSubmit(values)}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <Box
+                display="grid"
+                gap="30px"
+                marginTop={5}
+                gridTemplateColumns="repeat(2, minmax(0, 1fr))"
+              ></Box>
+              <FormControl
+                error={errors.currentPassword ? true : false}
+                sx={{ m: 1, minWidth: 120, gridColumn: "span 2" }}
+              >
+                <Field
+                  error={
+                    touched.currentPassword && errors.currentPassword
+                      ? true
+                      : false
+                  }
+                  as={TextField}
+                  label="Contraseña actual"
+                  type="password"
+                  name="currentPassword"
+                  fullWidth
+                />
+                <ErrorMessage
+                  name="currentPassword"
+                  component={() => (
+                    <FormHelperText>{errors.currentPassword}</FormHelperText>
+                  )}
+                />
+              </FormControl>
+              <FormControl
+                error={errors.newPassword ? true : false}
+                sx={{ m: 1, minWidth: 120, gridColumn: "span 2" }}
+              >
+                <Field
+                  error={
+                    touched.newPassword && errors.newPassword ? true : false
+                  }
+                  as={TextField}
+                  label="Nueva contraseña"
+                  type="password"
+                  name="newPassword"
+                  fullWidth
+                />
+                <ErrorMessage
+                  name="newPassword"
+                  component={() => (
+                    <FormHelperText>{errors.newPassword}</FormHelperText>
+                  )}
+                />
+              </FormControl>
+              <FormControl
+                error={errors.confirmNewPassword ? true : false}
+                sx={{ m: 1, minWidth: 120, gridColumn: "span 2" }}
+              >
+                <Field
+                  error={
+                    touched.confirmNewPassword && errors.confirmNewPassword
+                      ? true
+                      : false
+                  }
+                  as={TextField}
+                  label="Confirmar nueva contraseña"
+                  type="password"
+                  name="confirmNewPassword"
+                  fullWidth
+                />
+                <ErrorMessage
+                  name="confirmNewPassword"
+                  component={() => (
+                    <FormHelperText>{errors.confirmNewPassword}</FormHelperText>
+                  )}
+                />
+              </FormControl>
+              <DialogActions>
+                <Box display="flex" justifyContent="end" gap="10px">
+                  <Button
+                    color="neutral"
+                    variant="outlined"
+                    onClick={()=>handleCloseDialog(false)}
+                  >
+                    Cancelar
+                  </Button>
+                  <Button
+                    type="submit"
+                    color="secondary"
+                    variant="contained"
+                  >
+                    Crear
+                  </Button>
+                </Box>
+              </DialogActions>
+              {errorMessage && (
+                <Alert severity="error" sx={{ gridColumn: "span 2" }} fullWidth>
+                  {errorMessage}
+                </Alert>
+              )}
+            </Form>
+          )}
+        </Formik>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default FormUpdatePassword;
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -4,40 +4,30 @@ import {
   Menu,
   MenuItem as MuiMenuItem,
   useTheme,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-  TextField,
   useMediaQuery,
-  FormHelperText,
-  FormControl,
-  Alert,
-} from '@mui/material';
-import { useContext, useState } from 'react';
-import { ColorModeContext, tokens } from '../../theme';
-import InputBase from '@mui/material/InputBase';
-import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+} from "@mui/material";
+import { useContext, useState } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 /* import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'; */
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
-import SearchIcon from '@mui/icons-material/Search';
-import { MenuItem as ProSidebarMenuItem } from 'react-pro-sidebar';
-import { AuthContext } from '../login/AuthContext';
-import { useNavigate } from 'react-router-dom';
-import { useSnackbar } from 'notistack';
-import FormUser from './FormUser';
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import { AuthContext } from "../login/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
+import FormUser from "./FormUser";
+import FormUpdatePassword from "./FormUpdatePassword";
 
 const Topbar = () => {
+  const [openUpdatePasswordDialog, setOpenUpdatePasswordDialog] = useState(false);
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
 
-  const userRole = localStorage.getItem('userRole');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const userRole = localStorage.getItem("userRole");
+  const [errorMessage, setErrorMessage] = useState("");
   const [anchorEl, setAnchorEl] = useState(null);
   const [openDialog, setOpenDialog] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
@@ -53,72 +43,26 @@ const Topbar = () => {
   const { isAuthenticated, setAuthentication } = useContext(AuthContext);
   const handleLogout = () => {
     handleClose(); // Cerramos el menú
-    localStorage.removeItem('isAuthenticated');
+    localStorage.removeItem("isAuthenticated");
     setAuthentication(false);
-    navigate('/');
-  };
-
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Enviar la solicitud POST para crear un nuevo usuario
-    fetch('http://localhost:8000/api/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: username,
-        password: password,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.message == 'Usuario creado exitosamente') {
-          setUsername('');
-          setPassword('');
-          setErrorMessage('');
-          //alert('Usuario creado exitosamente');
-          handleCloseDialog();
-          enqueueSnackbar(data.message, { variant: 'success' });
-        } else {
-          setErrorMessage(data.message);
-          setTimeout(() => {
-            setErrorMessage(null);
-          }, 3000);
-        }
-      })
-      .catch((error) => {
-        console.error('Error al crear el usuario:', error);
-      });
-
-    setUsername('');
-    setPassword('');
-    setErrorMessage('');
-    //handleCloseDialog();
+    navigate("/");
   };
 
   const handleCreateUser = () => {
     setOpenDialog(true); // Abrir el diálogo de alta de usuarios
-    //console.log(username);
-    //console.log('handleCreateUser');
   };
 
   const handleCloseDialog = () => {
     setOpenDialog(false); // Cerrar el diálogo de alta de usuarios
-    setErrorMessage('');
+    setErrorMessage("");
   };
 
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const handleUpdatePassword = () => {
+    setOpenUpdatePasswordDialog(true);
+    handleClose(); // Cerrar el menú desplegable
+  };
 
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
@@ -137,7 +81,7 @@ const Topbar = () => {
       {/* ICONS */}
       <Box display="flex">
         <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === 'dark' ? (
+          {theme.palette.mode === "dark" ? (
             <DarkModeOutlinedIcon />
           ) : (
             <LightModeOutlinedIcon />
@@ -148,9 +92,9 @@ const Topbar = () => {
         </IconButton> */}
         <div>
           <IconButton
-            aria-controls={open ? 'basic-menu' : undefined}
+            aria-controls={open ? "basic-menu" : undefined}
             aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
+            aria-expanded={open ? "true" : undefined}
             onClick={handleClick}
           >
             <PersonOutlinedIcon />
@@ -161,8 +105,11 @@ const Topbar = () => {
             open={open}
             onClose={handleClose}
           >
+            <MuiMenuItem onClick={handleUpdatePassword}>
+              Cambiar contraseña
+            </MuiMenuItem>
             <MuiMenuItem onClick={handleLogout}>Cerrar sesión</MuiMenuItem>
-            {userRole === 'admin' && (
+            {userRole === "admin" && (
               <MuiMenuItem onClick={handleCreateUser}>
                 Crear usuario
               </MuiMenuItem>
@@ -170,73 +117,11 @@ const Topbar = () => {
           </Menu>
         </div>
       </Box>
-        <FormUser openDialog={openDialog} handleCloseDialog={handleCloseDialog} />
-{/*       <Dialog
-        open={openDialog}
-        onClose={handleCloseDialog}
-        fullScreen={isSmallScreen}
-        fullWidth
-      >
-        <DialogTitle
-          sx={{
-            background: '#444',
-            fontSize: '20px',
-            fontWeight: '600',
-          }}
-        >
-          Alta de usuario
-        </DialogTitle>
-        <DialogContent>
-          <form onSubmit={handleSubmit}>
-            <Box
-              display="grid"
-              gap="30px"
-              marginTop={5}
-              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-            >
-              <TextField
-                label="Nombre de usuario"
-                value={username}
-                onChange={handleUsernameChange}
-                fullWidth
-                sx={{ gridColumn: 'span 2' }}
-              />
-              <TextField
-                label="Contraseña"
-                type="password"
-                value={password}
-                onChange={handlePasswordChange}
-                fullWidth
-                sx={{ gridColumn: 'span 2' }}
-              />
-              {errorMessage && (
-                <Alert severity="error" sx={{ gridColumn: 'span 4' }} fullWidth>
-                  {errorMessage}
-                </Alert>
-              )}
-            </Box>
-            <DialogActions>
-              <Box
-                display="flex"
-                justifyContent="end"
-                gap="10px"
-                paddingTop={4}
-              >
-                <Button
-                  color="neutral"
-                  variant="outlined"
-                  onClick={handleCloseDialog}
-                >
-                  Cancelar
-                </Button>
-                <Button type="submit" color="secondary" variant="contained">
-                  Crear
-                </Button>
-              </Box>
-            </DialogActions>
-          </form>
-        </DialogContent>
-      </Dialog> */}
+      <FormUser openDialog={openDialog} handleCloseDialog={handleCloseDialog} />
+      <FormUpdatePassword
+        openDialog={openUpdatePasswordDialog}
+        handleCloseDialog={setOpenUpdatePasswordDialog}
+      />
     </Box>
   );
 };
